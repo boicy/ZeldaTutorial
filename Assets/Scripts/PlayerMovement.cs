b8c6ef603b253f169b0097892ed3f3e32aac679a
boicy <jamesa.lewis@gmail.com>
2020-05-08 16:52:10 +0100

    JAL: initial commit  video 20

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState {
+    walk,
+    attack,
+    interact
+}
+
+public class PlayerMovement : MonoBehaviour {
+
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start () {
+        currentState = PlayerState.walk;
+        myRigidBody = GetComponent<Rigidbody2D> ();
+        animator = GetComponent<Animator> ();
+        animator.SetFloat("moveX", 1);
+        animator.SetFloat("moveY", -1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw ("Horizontal");
+        change.y = Input.GetAxisRaw ("Vertical");
+        if (Input.GetButtonDown ("attack") && currentState != PlayerState.attack) {
+            StartCoroutine (AttackCoroutine ());
+        } else if (currentState == PlayerState.walk) {
+            UpdateAnimationAndMove ();
+        }
+    }
+
+    void UpdateAnimationAndMove () {
+        if (change != Vector3.zero) {
+            MovePlayer ();
+            animator.SetFloat ("moveX", change.x);
+            animator.SetFloat ("moveY", change.y);
+            animator.SetBool ("moving", true);
+        } else {
+            animator.SetBool ("moving", false);
+        };
+    }
+
+    void MovePlayer()
+    {
+        change.Normalize();
+        myRigidBody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+
+    private IEnumerator AttackCoroutine() {        
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool ("attacking", false);
+        yield return new WaitForSeconds (0.3f);
+        currentState = PlayerState.walk;
+    }
+
+
+}
---
+++ Assets/Scripts/Pots.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pots : MonoBehaviour
+{
+    private Animator amimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Smash()
+	{
+        amimator.SetBool("smash", true);
+        StartCoroutine(breakCoroutine());
+	}
+
+    IEnumerator breakCoroutine()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
