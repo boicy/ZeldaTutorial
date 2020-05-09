7404bbe4346e6a880c7ba030022b2c5ed6b2518e
boicy <jamesa.lewis@gmail.com>
2020-05-09 19:41:36 +0100

    JAL: beginning of 24

---
+++ Assets/Scripts/SignalListener.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalListener : MonoBehaviour
+{
+    public Signal signal;
+
+    public UnityEvent signalEvent;
+
+    public void OnSignalRaised()
+    {
+
+    }
+}
