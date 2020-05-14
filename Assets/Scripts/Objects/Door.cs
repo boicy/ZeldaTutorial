d26d4d2069c974fa9a10134d9754fe3709929896
boicy <jamesa.lewis@gmail.com>
2020-05-14 14:05:25 +0100

    JAL: added first keydoor with room transitions

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -13,16 +13,41 @@ public class Door : Interactable
     [Header("Door variables")]
     public DoorType thisDoorType;
     public bool open = false;
+    public Inventory playerInventory;
+    public SpriteRenderer doorSprite;
+    public BoxCollider2D physicsCollider;    
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (playerInRange && thisDoorType == DoorType.key)
+            {
+                //Does the player have a key?
+                if (playerInventory.numberOfKeys > 0)
+                {
+                    //Remove a key
+                    playerInventory.numberOfKeys--;
+                    //if so then call the open method
+                    Open();
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Open()
     {
-        
+        //Turn off the doors sprite renderer
+        doorSprite.enabled = false;
+        //set open to true
+        open = true;
+        //turn off the doors box collider
+        physicsCollider.enabled = false;
     }
+
+    public void Close()
+    {
+    }
+
+
 }
