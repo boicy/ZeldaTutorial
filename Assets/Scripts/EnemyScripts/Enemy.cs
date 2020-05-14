cb33bd7771b89d0615227b4e2334f78e42ebf074
boicy <jamesa.lewis@gmail.com>
2020-05-14 14:33:45 +0100

    JAL: reorganising prefabs and adding log death effect

--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -19,6 +19,7 @@ public class Enemy : MonoBehaviour
     public string enemyName;
     public int baseAttack;
     public float moveSpeed;
+    public GameObject deathEffect;
 
     private void Awake()
     {
@@ -30,7 +31,17 @@ public class Enemy : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
+            DeathEffect();
             this.gameObject.SetActive(false);
+        }
+    }
+
+    private void DeathEffect()
+    {
+        if (deathEffect!=null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1);
         }
     }
 
