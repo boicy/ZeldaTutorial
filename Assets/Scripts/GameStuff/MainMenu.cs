32d5b6d09ae61194b9b0a08d6197a03fad1863b1
boicy <jamesa.lewis@gmail.com>
2020-05-31 13:47:54 +0100

    JAL: Added a start menu - w00t

---
+++ Assets/Scripts/GameStuff/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private const string STARTING_SCENE = "Overworld";
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(STARTING_SCENE);
+    }
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+    }
+}
