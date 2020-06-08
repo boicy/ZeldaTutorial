72b091fc42104798cf8311a369c6317f0ea14ce6
boicy <jamesa.lewis@gmail.com>
2020-06-08 15:09:31 +0100

    A=JAL: Adding abilities but stopping here without changing player movement until I've sorted out the refactoring

---
+++ Assets/Scripts/ScriptableObjects/Abilities/DashAbility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Abilities/Dash Ability", fileName = "Dash Ability")]
+public class DashAbility : GenericAbility
+{
+    public float dashForce;
+
+    public override void Ability(Vector2 playerPosition, Vector2 playerFacingDirection = default,
+                                 Animator playerAnimator = null, Rigidbody2D playerRigidBody = null)
+    {
+        //Make sure enough magic
+        if (playerMagic.RuntimeValue >= magicCost)
+        {
+            playerMagic.RuntimeValue -= magicCost;
+            usePlayerMagic.Raise();
+        }
+        else
+        {
+            return;
+        }
+        if (playerRigidBody)
+        {
+            Vector3 dashVector = playerRigidBody.transform.position
+                + (Vector3)playerFacingDirection.normalized * dashForce;
+            playerRigidBody.DOMove(dashVector, duration);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Abilities/GenericAbility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Scriptable Objects/Abilities/Generic Ability", fileName ="New Generic Ability")]
+public class GenericAbility : ScriptableObject
+{
+
+    public float magicCost;
+    public float duration;
+
+    public FloatValue playerMagic;
+    public SignalSender usePlayerMagic;
+
+    public virtual void Ability(Vector2 playerPosition, Vector2 playerFacingDirection =  default(Vector2),
+        Animator playerAnimator = null, Rigidbody2D playerRigidBody = null)
+    {
+        
+    }
+
+}
